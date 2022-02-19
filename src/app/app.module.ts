7f712d7fafde636af5f9f506b657c8d375f7a0b4
Benoît Delplace <benoitdelplace@icloud.com>
2022-02-19 16:08:29 +0100

    Installation de JQuery côté Front-End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* -------------------------------------------------- */
+/* --------------------- ROUTES --------------------- */
+/* -------------------------------------------------- */
+
+import { Routes, RouterModule } from '@angular/router';
+
+/* -------------------------------------------------------- */
+/* --------------------- ARCHITECTURE --------------------- */
+/* -------------------------------------------------------- */
+
+/* ---- Main ----- */
+
+import { MainComponent } from './main/main.component';
+
+const appRoutes: Routes = [
+  { path: 'main', component: MainComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
