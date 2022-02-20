3fd8cae446d38e8731412b00484740d80d81c0c5
Benoît Delplace <benoitdelplace@icloud.com>
2022-02-20 15:58:38 +0100

    routes OK + install Bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
 
+/* ---- Main Navigation ----- */
+
+import { Part01Component } from './part01/part01.component';
+
 const routes: Routes = [
   { 
-    path: '', 
-    component: MainComponent
+    path: '', component: MainComponent
    },
   {
     path: '',
     redirectTo: '',
     pathMatch: 'full'
-  }
+  },
+  { path: 'part01', component: Part01Component }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 /* --------------------- MODULES --------------------- */
 /* -------------------------------------------------- */
 
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +12,8 @@ import { AppRoutingModule } from './app-routing.module';
 // everywhere in the app
 import { SharedModule } from "./shared/shared.module";
 // SharedModule needs CommonModule
-import { CommonModule } from "@angular/common";
+import { CommonModule, registerLocaleData } from "@angular/common";
+import * as fr from "@angular/common/locales/fr";
 
 /* -------------------------------------------------- */
 /* --------------------- ROUTES --------------------- */
@@ -28,16 +29,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
 
-/* ---- Navigation ----- */
+/* ---- Main Navigation ----- */
 
 import { Part01Component } from './part01/part01.component';
 
 const appRoutes: Routes = [
+
   { path: 'main', component: MainComponent },
   { path: '', redirectTo: 'main', pathMatch: 'full' },
-  { path: '**', redirectTo: 'main' },
+  { path: '**', redirectTo: 'main' }
   
-  { path: 'part01', component: Part01Component }
 ];
 
 @NgModule({
@@ -57,7 +58,13 @@ const appRoutes: Routes = [
   exports: [
     RouterModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
