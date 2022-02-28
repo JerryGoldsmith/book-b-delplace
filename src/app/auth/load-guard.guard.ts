4f68e3438edd408cbc563f37027d44ab488420ab
Benoît Delplace <benoitdelplace@icloud.com>
2022-02-28 18:58:14 +0100

    add load-guard-guard.ts for lazyLoading + delete assets > videos

---
+++ src/app/auth/load-guard.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class LoadGuardGuard implements CanLoad {
+    canLoad(
+        route: Route, 
+        segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        return true;
+    }
+
+}
