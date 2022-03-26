c125baee5418d36bc58a128c6c86f8c2a060cf27
Benoît Delplace <benoitdelplace@icloud.com>
2022-03-26 21:09:36 +0100

    signinlist > almost OK

--- src/app/auth/signinlist/signinlist.component.ts
+++ src/app/auth/signinlist/signinlist.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-// import custom validator to validate that password and confirm password fields match
+// custom validator to validate password and confirm password fields match
 import { ResizedEvent } from 'angular-resize-event';
 
 @Component({
   selector: 'app-signinlist',
   templateUrl: './signinlist.component.html',
-  styleUrls: ['./signinlist.component.scss']
+  styleUrls: ['./signinlist.component.scss',
+  '../../../assets/css/baseline-scroll/baseline-scroll-A.css',
+  '../../../assets/css/baseline-scroll/baseline-scroll-B.css',
+  '../../../assets/css/baseline-scroll/baseline-scroll-C.css']
 })
 export class SigninlistComponent implements OnInit {
 
@@ -26,6 +29,57 @@ export class SigninlistComponent implements OnInit {
 
   ngOnInit(): void {
 
+    function showBaselineA(el: string) {
+      var windowHeight = jQuery( window ).height();
+      $(el).each(function(){
+          var position = $(this).offset().top;
+
+          var topOfWindow = $(window).scrollTop();
+          if (topOfWindow + windowHeight - 200 > position ) {
+              $(this).addClass("fadeIn");
+          }
+      });
+    }
+
+    $(window).on('scroll', function () {
+      showBaselineA('.baseline-A');
+    });
+
+    // -------
+
+    function showBaselineB(el: string) {
+      var windowHeight = jQuery( window ).height();
+      $(el).each(function(){
+          var position = $(this).offset().top;
+
+          var topOfWindow = $(window).scrollTop();
+          if (topOfWindow + windowHeight - 200 > position ) {
+              $(this).addClass("fadeIn");
+          }
+      });
+    }
+
+    $(window).on('scroll', function () {
+      showBaselineB('.baseline-B');
+    });
+
+    // --------
+
+    function showBaselineC(el: string) {
+      var windowHeight = jQuery( window ).height();
+      $(el).each(function(){
+          var position = $(this).offset().top;
+
+          var topOfWindow = $(window).scrollTop();
+          if (topOfWindow + windowHeight - 200 > position ) {
+              $(this).addClass("fadeIn");
+          }
+      });
+    }
+
+    $(window).on('scroll', function () {
+      showBaselineC('.baseline-C');
+    });
   }
 
 }
