c74732aca97a8a280a461447da390cd2ce571b9f
Benoît Delplace <benoitdelplace@icloud.com>
2022-05-01 20:22:20 +0200

    charts.js & firestore with highchart OK with customerAge

--- src/app/order-reservation-list-query-graphic-bar/order-reservation-list-query-graphic-bar.component.ts
+++ src/app/order-reservation-list-query-graphic-bar/order-reservation-list-query-graphic-bar.component.ts
@@ -60,11 +60,11 @@ export class OrderReservationListQueryGraphicBarComponent implements OnInit {
 
     // ----
 
-    this.highchartservice.rates$.subscribe((assets) => {
+    this.highchartservice.customerAge$.subscribe((assets) => {
       this.items$ = assets;
       if (this.items$) {
         this.items$.forEach((element) => {
-          this.chardata.push(element.rate);
+          this.chardata.push(element.customerAge);
         });
         this.getChart();
       }
@@ -102,12 +102,54 @@ export class OrderReservationListQueryGraphicBarComponent implements OnInit {
                 backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1
               }]
--- src/app/services/highchart.service.ts
+++ src/app/services/highchart.service.ts
@@ -8,8 +8,8 @@ import { map } from "rxjs/operators";
 })
   export class HighchartService {
 
-    private rateCollection: AngularFirestoreCollection < chartModal > ;
-    rates$: Observable < chartModal[] > ;
+    // private rateCollection: AngularFirestoreCollection < chartModal > ;
+    // rates$: Observable < chartModal[] > ;
     
     private customerAgeCollection: AngularFirestoreCollection < chartModal > ;
     customerAge$: Observable < chartModal[] > ;
@@ -17,21 +17,12 @@ import { map } from "rxjs/operators";
     constructor(
         private readonly firestoreservice: AngularFirestore
         ) {
-      this.rateCollection = firestoreservice.collection < chartModal > ('seatOneOrders');
+      this.customerAgeCollection = firestoreservice.collection < chartModal > ('seatOneOrders');
       // .snapshotChanges() returns a DocumentChangeAction[], which contains
       // a lot of information about "what happened" with each change. If you want to
       // get the data and the id use the map operator.
-      this.rates$ = this.rateCollection.snapshotChanges().pipe(
-        map(actions => actions.map(a => {
-          const data = a.payload.doc.data() as chartModal;
-          const id = a.payload.doc.id;
-          return {
-            id,
-            ...data
-          };
-        }))
-      );
-    //   this.customerAge$ = this.customerAgeCollection.snapshotChanges().pipe(
+
+    //   this.rates$ = this.rateCollection.snapshotChanges().pipe(
     //     map(actions => actions.map(a => {
     //       const data = a.payload.doc.data() as chartModal;
     //       const id = a.payload.doc.id;
@@ -41,10 +32,20 @@ import { map } from "rxjs/operators";
     //       };
     //     }))
     //   );
+      this.customerAge$ = this.customerAgeCollection.snapshotChanges().pipe(
+        map(actions => actions.map(a => {
+          const data = a.payload.doc.data() as chartModal;
+          const id = a.payload.doc.id;
+          return {
+            id,
+            ...data
+          };
+        }))
+      );
     }
   }
   export interface chartModal {
     currency: string,
-      rate: number
-    //   customerAge: number
+    //   rate: number
+      customerAge: number
   }
