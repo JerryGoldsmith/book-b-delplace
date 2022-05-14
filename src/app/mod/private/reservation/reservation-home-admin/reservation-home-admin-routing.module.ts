1448b35d54db419356b201a75da0d3b863d3c140
Benoît Delplace <benoitdelplace@icloud.com>
2022-05-14 15:38:36 +0200

    reservation > lazy loading almost OK

--- src/app/mod/private/reservation/reservation-home-admin/reservation-home-admin-routing.module.ts
+++ src/app/mod/private/reservation/reservation-home-admin/reservation-home-admin-routing.module.ts
@@ -5,6 +5,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { ReservationHomeAdminComponent } from 'src/app/part01-reservation/reservation-home-admin/reservation-home-admin.component';
 
+import { KeyValuePipeModule } from 'src/app/routing/key-value-pipe.module';
+import { ValueArrayPipeModule } from 'src/app/routing/value-array-pipe.module';
+
 const routes: Routes = [
   { path: '', component: ReservationHomeAdminComponent }
 ];
@@ -12,7 +15,9 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forChild(routes),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    KeyValuePipeModule,
+    ValueArrayPipeModule
   ],
   exports: [
     RouterModule
--- src/app/mod/private/reservation/reservation-home-admin/reservation-home-admin.module.ts
+++ src/app/mod/private/reservation/reservation-home-admin/reservation-home-admin.module.ts
@@ -7,24 +7,134 @@ import { ReservationHomeAdminComponent } from 'src/app/part01-reservation/reserv
 import { ReservationAdminComponent } from 'src/app/part01-reservation/reservation-admin/reservation-admin.component';
 import { ReservationQueryDateComponent } from 'src/app/part01-reservation/reservation-query-date/reservation-query-date.component';
 
+import { SeatsComponent } from 'src/app/part01-reservation/seats/seats/seats.component';
+
+import { ButtonAAComponent } from 'src/app/part01-reservation/buttons-delete/button-aa/button-aa.component';
+import { ButtonBAComponent } from 'src/app/part01-reservation/buttons-delete/button-ba/button-ba.component';
+import { ButtonCAComponent } from 'src/app/part01-reservation/buttons-delete/button-ca/button-ca.component';
+import { ButtonDAComponent } from 'src/app/part01-reservation/buttons-delete/button-da/button-da.component';
+import { ButtonEAComponent } from 'src/app/part01-reservation/buttons-delete/button-ea/button-ea.component';
+import { ButtonFAComponent } from 'src/app/part01-reservation/buttons-delete/button-fa/button-fa.component';
+import { ButtonGAComponent } from 'src/app/part01-reservation/buttons-delete/button-ga/button-ga.component';
+import { ButtonHAComponent } from 'src/app/part01-reservation/buttons-delete/button-ha/button-ha.component';
+import { ButtonIAComponent } from 'src/app/part01-reservation/buttons-delete/button-ia/button-ia.component';
+import { ButtonJAComponent } from 'src/app/part01-reservation/buttons-delete/button-ja/button-ja.component';
+import { ButtonKAComponent } from 'src/app/part01-reservation/buttons-delete/button-ka/button-ka.component';
+import { ButtonLAComponent } from 'src/app/part01-reservation/buttons-delete/button-la/button-la.component';
+import { ButtonMAComponent } from 'src/app/part01-reservation/buttons-delete/button-ma/button-ma.component';
+import { ButtonNAComponent } from 'src/app/part01-reservation/buttons-delete/button-na/button-na.component';
+import { ButtonOAComponent } from 'src/app/part01-reservation/buttons-delete/button-oa/button-oa.component';
+import { ButtonPAComponent } from 'src/app/part01-reservation/buttons-delete/button-pa/button-pa.component';
+import { ButtonQAComponent } from 'src/app/part01-reservation/buttons-delete/button-qa/button-qa.component';
+import { ButtonRAComponent } from 'src/app/part01-reservation/buttons-delete/button-ra/button-ra.component';
+import { ButtonSAComponent } from 'src/app/part01-reservation/buttons-delete/button-sa/button-sa.component';
+import { ButtonTAComponent } from 'src/app/part01-reservation/buttons-delete/button-ta/button-ta.component';
+import { ButtonUAComponent } from 'src/app/part01-reservation/buttons-delete/button-ua/button-ua.component';
+import { ButtonVAComponent } from 'src/app/part01-reservation/buttons-delete/button-va/button-va.component';
+import { ButtonWAComponent } from 'src/app/part01-reservation/buttons-delete/button-wa/button-wa.component';
+import { ButtonXAComponent } from 'src/app/part01-reservation/buttons-delete/button-xa/button-xa.component';
+import { ButtonYAComponent } from 'src/app/part01-reservation/buttons-delete/button-ya/button-ya.component';
+import { ButtonZAComponent } from 'src/app/part01-reservation/buttons-delete/button-za/button-za.component';
+import { ButtonABComponent } from 'src/app/part01-reservation/buttons-delete/button-ab/button-ab.component';
+import { ButtonBBComponent } from 'src/app/part01-reservation/buttons-delete/button-bb/button-bb.component';
+import { ButtonCBComponent } from 'src/app/part01-reservation/buttons-delete/button-cb/button-cb.component';
+import { ButtonDBComponent } from 'src/app/part01-reservation/buttons-delete/button-db/button-db.component';
+import { ButtonEBComponent } from 'src/app/part01-reservation/buttons-delete/button-eb/button-eb.component';
+import { ButtonFBComponent } from 'src/app/part01-reservation/buttons-delete/button-fb/button-fb.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { KeyValuePipeModule } from 'src/app/routing/key-value-pipe.module';
+import { ValueArrayPipeModule } from 'src/app/routing/value-array-pipe.module';
 
 @NgModule({
   declarations: [
     ReservationHomeAdminComponent,
     ReservationAdminComponent,
-    ReservationQueryDateComponent
+    ReservationQueryDateComponent,
+    SeatsComponent,
+
+    ButtonAAComponent,
+    ButtonBAComponent,
+    ButtonCAComponent,
+    ButtonDAComponent,
+    ButtonEAComponent,
+    ButtonFAComponent,
+    ButtonGAComponent,
+    ButtonHAComponent,
+    ButtonIAComponent,
+    ButtonJAComponent,
+    ButtonKAComponent,
+    ButtonLAComponent,
+    ButtonMAComponent,
+    ButtonNAComponent,
+    ButtonOAComponent,
+    ButtonPAComponent,
+    ButtonQAComponent,
+    ButtonRAComponent,
+    ButtonSAComponent,
+    ButtonTAComponent,
+    ButtonUAComponent,
+    ButtonVAComponent,
+    ButtonWAComponent,
+    ButtonXAComponent,
+    ButtonYAComponent,
+    ButtonZAComponent,
+
+    ButtonABComponent,
+    ButtonBBComponent,
+    ButtonCBComponent,
+    ButtonDBComponent,
+    ButtonEBComponent,
+    ButtonFBComponent
   ],
   imports: [
     CommonModule,
     ReservationHomeAdminRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    KeyValuePipeModule,
+    ValueArrayPipeModule
   ],
   exports: [
     ReservationHomeAdminComponent,
     ReservationAdminComponent,
-    ReservationQueryDateComponent
+    ReservationQueryDateComponent,
+    SeatsComponent,
+
+    ButtonAAComponent,
+    ButtonBAComponent,
+    ButtonCAComponent,
+    ButtonDAComponent,
+    ButtonEAComponent,
+    ButtonFAComponent,
+    ButtonGAComponent,
+    ButtonHAComponent,
+    ButtonIAComponent,
+    ButtonJAComponent,
+    ButtonKAComponent,
+    ButtonLAComponent,
+    ButtonMAComponent,
+    ButtonNAComponent,
+    ButtonOAComponent,
+    ButtonPAComponent,
+    ButtonQAComponent,
+    ButtonRAComponent,
+    ButtonSAComponent,
+    ButtonTAComponent,
+    ButtonUAComponent,
+    ButtonVAComponent,
+    ButtonWAComponent,
+    ButtonXAComponent,
+    ButtonYAComponent,
+    ButtonZAComponent,
+
+    ButtonABComponent,
+    ButtonBBComponent,
+    ButtonCBComponent,
+    ButtonDBComponent,
+    ButtonEBComponent,
+    ButtonFBComponent
   ]
 })
 export class ReservationHomeAdminModule { }
